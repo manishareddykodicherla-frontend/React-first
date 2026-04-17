04f1866ae8e2f90d92f0e264037bb74f5202df2c
manishareddykodicherla-frontend <manishareddykodicherla@gmail.com>
2026-04-16 17:57:53 -0700

    header completed

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import Nav from './Components/Nav.jsx';
 import Footer from './Components/Footer.jsx';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './Pages/Home.jsx'
+import Books from './Pages/Books.jsx'
 function App() {
   return (
     <Router>
    <div clasName="App">
       <Nav/>
       <Switch >
-      <Route path="/home" exact Component={<Home/>}/> 
+      <Route path="/" exact component={Home}/> 
+      <Route path="/books" components={Books}/>
       </Switch>
       <Footer/>
     </div>
